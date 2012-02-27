c8c4188ae45d4a0b800a026943fe6cd731a480a8
Enric Florit <efz1005@gmail.com>
2012-02-27 22:35:44 +0100

    More minor changes

--- juri.orig.js
+++ juri.orig.js
@@ -14,16 +14,250 @@ var jUri = (function(window,document){
         window.console.error = window.console.warn = window.console.debug = window.console.log;
     }
 
+    var loc = window.location,
+    host = loc.host,
+    hostname = loc.hostname,
+    href = loc.href,
+    pathname = loc.pathname,
+    port = loc.port,
+    protocol = loc.protocol.replace(/:$/,''),
+    get = (function(obj){
+
+        var getStr = obj.replace(/^\?/,''),
+        getVars = getStr.split('&'),
+        json = {};
+        
+        for( var e in getVars ){
+            var getParam = getVars[e].split('=');
+            json[ getParam[0] ] = getParam[1];
+        }
+
+        return json;
+
+    })(loc.search);
+
     return {
         
-        host: window.location.host,
-        hostname: window.location.hostname,
-        href: window.location.href,
-        pathname: window.location.pathname,
-        port: window.location.port,
-        protocol: window.location.protocol.replace(/:$/,''),
-        get: window.location.search,
+        host: host,
+        hostname: hostname,
+        href: href,
+        pathname: pathname,
+        port: port,
+        protocol: protocol,
+        get: get,
         
+        /* jUri.hash();// returns the current hash
+        jUri.hash(/regexp/);// returns true/false
+        jUri.hash(/regexp/, function(){// returns true/false, executes callback if true
+        });
+        jUri.hash('new hash');// changes the hash and returns the new value
+        */
+           
+        hash: function( text, callback ){
+            if( text && text instanceof( RegExp ) ){
+                if( window.location.hash.replace(/^#/,'').match( text ) ){
+                    callback ? callback() : false;
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+            
+            else if( text ){
+                window.location.hash = text;
+                return text;
+            }
+            
+            else{
+                return window.location.hash.replace(/^#/,'');
+            }
+        },
+        
+        
+        /* jUri.hashchange(function(e){
+        //bind a function to the window.onhashchange event:
+        alert('The previous hash was "'+e.oldHash+'" and now it\'s "'+e.newHash+'"');
+        });
+        */
+        
+        hashchange: function( callback ){
+            if( !document.body ){
+                return setTimeout('jUri.hashchange(' + callback +')', 1);
+            }
+            
+            var scroll = 0;
+            if( typeof window.pageYOffset == 'number' ) {
+                //Netscape compliant
+                scroll = window.pageYOffset;
+            } else if( document.body.scrollTop ) {
+                //DOM compliant
+                scroll = document.body.scrollTop;
+            } else if( document.documentElement.scrollTop ) {
+                //IE6 standards compliant mode
+                scroll = document.documentElement.scrollTop;
+            }
+            
+            this.fn.checkhash( this.hash(), scroll, callback );
+        },
+        
+        
+        /* jUri.isHTTP();// returns true/false
+        jUri.isHTTP(function(){// returns true/false, executes callback if true
+        });
+        */
+        
+        isHTTP: function( callback ){
+            if( this.protocol.match(/^http$/) ){
+                callback ? callback() : false;
+                return true;
+            }else{
+                return false;
+            }
+        },
+        
+        
+        /* jUri.isHTTPS();// returns true/false
+        jUri.isHTTPS(function(){// returns true/false, executes callback if true
+        });
+        */
+        
+        isHTTPS: function( callback ){
+            if( this.protocol.match(/^https$/) ){
+                callback ? callback() : false;
+                return true;
+            }else{
+                return false;
+            }
+        },
+
+
+        parseAsGet: function( str ){
+            var getVars = (str+'').replace(/^(.*?)\?/,'').split('&'),
+            
+            json = {};
+            
+            for( var e in getVars ){
+                var getParam = getVars[e].split('=');
+                json[ getParam[0] ] = getParam[1];
+            }
+
+            return json;
+        },
+
+
+        /* jUri.redirect('index.html');// goes to the required url
+        jUri.redirect('index.html', 1000);// goes to the required time after 1000 ms
+        */
+        
+        redirect: function( uri, timeout ){
+            if( timeout && timeout % 1 === 0 ){
+                setTimeout("window.location.href='" + uri + "'", timeout);
+            }else{
+                window.location.href = uri;
+            }
+        },
+
+
+        /* jUri.reload();//reloads the page
+        jUri.reload(1000);//reloads the page in 1000 ms
+        */
+
+        reload: function( timeout ){
+            if( timeout && timeout % 1 === 0 ){
+                setTimeout("window.location.reload(true);", timeout);
+            }else{
+                window.location.reload(true);
+            }
+        },
+        
+        
+        /* jUri.isFile();// returns true/false if the current url is a file path
+        jUri.isFile(function(){// executes callback if true, returns true/false
+        });
+        //NOTE: does not check if the file exists
+        */
+        
+        isFile: function( callback ){
+            if( this.pathname.match(/\.(html|htm|php|phtml|asp|shtml|cgi|jsp|pl)$/i) ){
+                callback ? callback() : false;
+                return true;
+            }else{
+                return false;
+            }
+        },
+        
+        
+        /* jUri.anchorExists('name');// returns true/false if there is an <a> tag in the document
+        // with this code: <a name="name"></a>
+        */
+        
+        anchorExists: function( hash ){
+            var anchor = this.fn.getAnchor( hash );
+            if( anchor ){
+               return true;
+            }
+            
+            return false;
+        },
+        
+        
+        animateAnchorLinks: function( anchors, changeHash ){
+            if( !document.body ){
+                return setTimeout('jUri.animateAnchorLinks("' + anchors + '")',1);
+            }
+
+            if( !changeHash || typeof changeHash != 'bool'){
+                changeHash = true;
+            }
+
+            var a = [],
+            linkList = [],
+            links = document.getElementsByTagName('a');
+            
+            if( !anchors || typeof anchors == 'undefined' || anchors === 'undefined' ){
+                for( var e in links ){
+                    if( links[e].name && links[e].name != '' ){
+                        a.push(links[e].name);
+                    }
+                }
+            }else{
+                var splitted = anchors.replace(/\s/gim,'').split(','),
+                anchor;
+                
+                for( var e in splitted ){
+                    anchor = splitted[e];
+                    a.push( this.fn.getAnchor( anchor ).name );
+                }
+            }
+            
+            //iterate the 'a' array and match all the links refering to each anchor
+            for( var i in a ){
+                for( var e in links ){
+                    if( links[e].href && links[e].href != '' && links[e].href.match('#'+a[i]+'$') ){
+                        linkList.push( links[e] );
+                    }
+                }
+            }
+            
+            //iterate the linkList and bind a click event to each link
+            for( var i in linkList ){
+                linkList[i].onclick = function(e){
+                    //Disable default scrolling
+                    e.preventDefault();
+
+                    var anchorName = this.href.split('#')[1];
+                    jUri.fn.gotoAnchor( anchorName, function(){
+                        if(changeHash) jUri.hash(anchorName);
+                    });
+                    
+                }
+            }
+        },
+
+        encode: function(str){
+            return encodeURIComponent(str);
+        },
+
         fn: {
             /* jUri.fn.checkhash();
             USED BY jUri.hashchange() event
@@ -169,241 +403,6 @@ var jUri = (function(window,document){
                     jUri.fx.scroller.callback = null;
                 }
             }
-        },
-
-
-        /* jUri.hash();// returns the current hash
-        jUri.hash(/regexp/);// returns true/false
-        jUri.hash(/regexp/, function(){// returns true/false, executes callback if true
-        });
-        jUri.hash('new hash');// changes the hash and returns the new value
-        */
-           
-        hash: function( text, callback ){
-            if( text && text instanceof( RegExp ) ){
-                if( window.location.hash.replace(/^#/,'').match( text ) ){
-                    callback ? callback() : false;
-                    return true;
-                }else{
-                    return false;
-                }
-            }
-            
-            else if( text ){
-                window.location.hash = text;
-                return text;
-            }
-            
-            else{
-                return window.location.hash.replace(/^#/,'');
-            }
-        },
-        
-        
-        /* jUri.hashchange(function(e){
-        //bind a function to the window.onhashchange event:
-        alert('The previous hash was "'+e.old+'" and now it\'s "'+e.new+'"');
-        });
-        */
-        
-        hashchange: function( callback ){
-            if( !document.body ){
-                return setTimeout('jUri.hashchange(' + callback +')', 1);
-            }
-            
-            var scroll = 0;
-            if( typeof window.pageYOffset == 'number' ) {
-                //Netscape compliant
-                scroll = window.pageYOffset;
-            } else if( document.body.scrollTop ) {
-                //DOM compliant
-                scroll = document.body.scrollTop;
-            } else if( document.documentElement.scrollTop ) {
-                //IE6 standards compliant mode
-                scroll = document.documentElement.scrollTop;
-            }
-            
-            jUri.fn.checkhash( jUri.hash(), scroll, callback );
-        },
-        
-        
-        /* jUri.isHTTP();// returns true/false
-        jUri.isHTTP(function(){// returns true/false, executes callback if true
-        });
-        */
-        
-        isHTTP: function( callback ){
-            if( this.protocol.match(/^http$/) ){
-                callback ? callback() : false;
-                return true;
-            }else{
-                return false;
-            }
-        },
-        
-        
-        /* jUri.isHTTPS();// returns true/false
-        jUri.isHTTPS(function(){// returns true/false, executes callback if true
-        });
-        */
-        
-        isHTTPS: function( callback ){
-            if( this.protocol.match(/^https$/) ){
-                callback ? callback() : false;
-                return true;
-            }else{
-                return false;
-            }
-        },
-        
-        
-        /* jUri.vars();// returns a map with all vars passed by get
-        jUri.vars('key');// returns the value of the required key,
-        if none, returns the entire map
-        */
-        
-        vars: function( index ){
-            var getStr = this.get.replace(/^\?/,''),
-            getVars = getStr.split('&'),
-            
-            json = {};
-            
-            for( var e in getVars ){
-                var getParam = getVars[e].split('=');
-                json[ getParam[0] ] = getParam[1];
-            }
-            
-            if( index && json[ index ] ) return json[ index ];
-
-            return json;
-        },
-
-
-        parseAsGet: function( str ){
-            var getVars = (str+'').replace(/^(.*?)\?/,'').split('&'),
-            
-            json = {};
-            
-            for( var e in getVars ){
-                var getParam = getVars[e].split('=');
-                json[ getParam[0] ] = getParam[1];
-            }
-
-            return json;
-        },
-
-
-        /* jUri.redirect('index.html');// goes to the required url
-        jUri.redirect('index.html', 1000);// goes to the required time after 1000 ms
-        */
-        
-        redirect: function( uri, timeout ){
-            if( timeout && timeout % 1 === 0 ){
-                setTimeout("window.location.href='" + uri + "'", timeout);
-            }else{
-                window.location.href = uri;
-            }
-        },
-
-
-        /* jUri.reload();//reloads the page
-        jUri.reload(1000);//reloads the page in 1000 ms
-        */
-
-        reload: function( timeout ){
-            if( timeout && timeout % 1 === 0 ){
-                setTimeout("window.location.reload(true);", timeout);
-            }else{
-                window.location.reload(true);
-            }
-        },
-        
-        
-        /* jUri.isFile();// returns true/false if the current url is a file path
-        jUri.isFile(function(){// executes callback if true, returns true/false
-        });
-        //NOTE: does not check if the file exists
-        */
-        
-        isFile: function( callback ){
-            if( this.pathname.match(/\.(html|htm|php|phtml|asp|shtml|cgi|jsp|pl)$/i) ){
-                callback ? callback() : false;
-                return true;
-            }else{
-                return false;
-            }
-        },
-        
-        
-        /* jUri.anchorExists('name');// returns true/false if there is an <a> tag in the document
-        // with this code: <a name="name"></a>
-        */
-        
-        anchorExists: function( hash ){
-            var anchor = this.fn.getAnchor( hash );
-            if( anchor ){
-               return true;
-            }
-            
-            return false;
-        },
-        
-        
-        animateAnchorLinks: function( anchors, changeHash ){
-            if( !document.body ){
-                return setTimeout('jUri.animateAnchorLinks("' + anchors + '")',1);
-            }
-
-            if( !changeHash || typeof changeHash != 'bool'){
-                changeHash = true;
-            }
-
-            var a = [],
-            linkList = [],
-            links = document.getElementsByTagName('a');
-            
-            if( !anchors || typeof anchors == 'undefined' || anchors === 'undefined' ){
-                for( var e in links ){
-                    if( links[e].name && links[e].name != '' ){
-                        a.push(links[e].name);
-                    }
-                }
-            }else{
-                var splitted = anchors.replace(/\s/gim,'').split(','),
-                anchor;
-                
-                for( var e in splitted ){
-                    anchor = splitted[e];
-                    a.push( jUri.fn.getAnchor( anchor ).name );
-                }
-            }
-            
-            //iterate the 'a' array and match all the links refering to each anchor
-            for( var i in a ){
-                for( var e in links ){
-                    if( links[e].href && links[e].href != '' && links[e].href.match('#'+a[i]+'$') ){
-                        linkList.push( links[e] );
-                    }
-                }
-            }
-            
-            //iterate the linkList and bind a click event to each link
-            for( var i in linkList ){
-                linkList[i].onclick = function(e){
-                    //Disable default scrolling
-                    e.preventDefault();
-
-                    var anchorName = this.href.split('#')[1];
-                    jUri.fn.gotoAnchor( anchorName, function(){
-                        if(changeHash) jUri.hash(anchorName);
-                    });
-                    
-                }
-            }
-        },
-
-        encode: function(str){
-            return encodeURIComponent(str);
         },
 
         log: function(str){
